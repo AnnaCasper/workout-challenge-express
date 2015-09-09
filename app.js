025f876b15e31d310c4b2a4cdd7098fbea5ca45e
Anna Casper <anna.casper3@gmail.com>
2015-09-09 17:15:14 -0600

    added lines for deployment

--- app.js
+++ app.js
@@ -48,6 +48,11 @@ if (app.get('env') === 'development') {
   });
 }
 
+//for use with deployment
+if (app.get('env') === 'production') {
+    app.listen(3000);
+  }
+
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
